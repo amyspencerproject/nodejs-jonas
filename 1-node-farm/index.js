7ddc2428c232b54a4ba29203283f3c802e57bdd3
amyspencerproject <amy@spencerproject.com>
2023-06-07 12:32:43 -0600

    Replaced some of the placeholders in the HTML templates but need to debug

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -29,8 +29,23 @@ const url = require('url');
 // console.log('Will read file!');
 
 //  SERVER *****************
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+        return output;
+    }
+}
+
 // HTML templates
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8',);
+const tempCard = fs .readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8',);
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8',);
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8',);
 
@@ -48,7 +63,10 @@ const server = http.createServer((req, res) => {
     // Overview Page
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html'});
+        const cardsHTML = dataObj.map( element => replaceTemplate (tempCard, element) );
+        console.log(cardsHTML);
         res.end(tempOverview);
+       
 
     // Product page
     } else if (pathName === '/product') {
