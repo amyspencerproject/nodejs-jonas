0cca29b9175b100f43612febf5d486aa6ff056a3
amyspencerproject <amy@spencerproject.com>
2023-06-14 17:14:16 -0600

    Trying to replace product slugs for id in url

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./starter/modules/replaceTemplate');
 
 //  FILES *****************
@@ -42,12 +43,10 @@ const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-pro
 // Data
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8',);
 const dataObj = JSON.parse(data); 
-
-
+const slugs = dataObj.map( element => slugify(element.productName, {lower: true}));
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse (req.url, true)
-    // const pathName = req.url;
 
 
     // Overview Page
@@ -61,7 +60,7 @@ const server = http.createServer((req, res) => {
     // Product page
     } else if (pathname === '/product') {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        const product = dataObj[query.id];
+        const product = dataObj[query.id]
         const output = replaceTemplate(tempProduct, product);
         res.end(output);
 
