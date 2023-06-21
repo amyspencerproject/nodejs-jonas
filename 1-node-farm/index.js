4caa301c4b7d566195d4b6e4dcaf85f2761321ae
amyspencerproject <amy@spencerproject.com>
2023-06-20 20:59:35 -0600

    Setup Prettier as the default formatter upon saving a file

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -44,6 +44,7 @@ const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-pro
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8',);
 const dataObj = JSON.parse(data); 
 const slugs = dataObj.map( element => slugify(element.productName, {lower: true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse (req.url, true)
